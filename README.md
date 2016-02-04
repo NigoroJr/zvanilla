064c70bb90251185b02089ef29dab99103be1453
Naoki Mizuno <nigorojr@gmail.com>
2016-02-03 20:31:23 -0500

    Initial import

---
+++ README.md
@@ -0,0 +1,28 @@
+# zvanilla
+
+Start a zsh session with a different `.zshrc`.
+
+## Usage
+
+```
+zsh zvanilla.zsh [-h] [-s <shell command>] [-t <tmpdir>] [zshrc]
+```
+
+Without a file, it will start a zsh session with no initial configurations.
+
+## Example
+```
+% cat my_zshrc.zsh
+echo 'Hello, World!'
+PROMPT='>>> '
+% zsh zvanilla.zsh -s /usr/local/bin/zsh -t . my_zshrc.zsh
+Hello, World!
+>>> exit
+%
+```
+
+## License
+MIT
+
+## Author
+Naoki Mizuno
---
+++ zvanilla.zsh
@@ -0,0 +1,38 @@
+#!/usr/bin/env zsh
+
+typeset -gx ZVANILLA_DIR
+
+local dir
+local shell
+local zshrc
+
+while getopts 'hs:t:' flag; do
+    case "$flag" in
+        s)
+            shell=$OPTARG
+            ;;
+        t)
+            dir=$OPTARG
+            ;;
+        h)
+            echo "Usage: $0 [-h] [-s <shell command>] [-t <tmpdir>] <zshrc>"
+            exit 0
+    esac
+
+done
+shift $(( $OPTIND - 1 ))
+
+# Use TMPDIR or current directory
+: ${dir:=${TMPDIR:-.}}
+# Use $SHELL or /bin/zsh
+: ${shell:=${SHELL:-/bin/zsh}}
+
+: ${ZVANILLA_DIR:=$( mktemp -d "$dir/zvanilla_XXXX" )}
+zshrc="$1"
+
+if [[ -n $zshrc ]]; then
+    cp $zshrc $ZVANILLA_DIR/.zshrc
+fi
+ZDOTDIR=$ZVANILLA_DIR $shell
+
+rm -r $ZVANILLA_DIR
